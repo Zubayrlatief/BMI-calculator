696c4ea31ea74e6babfbc986d95620e138de15a3
zubayrlatief <zubayrlatie007@gmail>
2024-05-17 23:01:54 +0200

    firstcommit

---
+++ javascript.js
@@ -0,0 +1,31 @@
+document.getElementById('calculateButton').addEventListener('click', function () {
+    let weight = parseFloat(document.getElementById('weight').value);
+    let height = parseFloat(document.getElementById('height').value);
+
+    if (!isNaN(weight) && !isNaN(height) && height > 0) {
+        const bmi = weight / ((height / 100) * (height / 100));
+        const resultElement = document.getElementById('result');
+
+        let category;
+        if (bmi < 18.5) {
+            category = 'Underweight';
+        } else if (bmi >= 18.5 && bmi < 25) {
+            category = 'Normal Weight';
+        } else {
+            category = 'Overweight';
+        }
+
+        let comment;
+        if (bmi < 18.5) {
+            comment = 'bulk';
+        } else if (bmi >= 18.5 && bmi < 25) {
+            comment = 'healthy';
+        } else {
+            comment = 'cut';
+        }
+
+        resultElement.innerHTML = 'Your BMI is: ' + bmi.toFixed(2) + '<br>Category: ' + category + '<br>Comment: ' + comment;
+        } else {
+        document.getElementById('result').innerHTML = 'Please enter valid weight and height.';
+    }
+});
